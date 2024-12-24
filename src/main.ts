daef3a5a9c7f8bf89ce5166d581f30d9a56b2bcc
Surendra Wagle <cirrusminor2@gmail.com>
2024-12-24 12:13:26 +0545

    main-file-changed-now

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
     //app.enableCors();
     app.enableCors({
-        origin: 'http://localhost:5173', // Replace with your frontend URL
+        origin: 'http://3.12.155.227:5173', // Replace with your frontend URL
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true, // Ensure credentials are allowed
     });
@@ -33,20 +33,25 @@ async function bootstrap() {
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document, {
-        customSiteTitle: 'Backend Generator',
-        customfavIcon:
-            'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
-        customJs: [
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-        ],
-        customCssUrl: [
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-        ],
-    });
+    SwaggerModule.setup(
+        'api',
+        app,
+        document,
+        //     {
+        //     customSiteTitle: 'Backend Generator',
+        //     customfavIcon:
+        //         'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+        //     customJs: [
+        //         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+        //         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+        //     ],
+        //     customCssUrl: [
+        //         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+        //         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+        //         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        //     ],
+        // }
+    );
 
     await app.listen(5000);
 }
